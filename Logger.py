83fe16fa0821d3971294e5ef27ec66a638c85016
Rahul Sampagaon <rahulsamp4@gmail.com>
2023-10-24 20:11:12 -0700

    code completed - documentation remaining

--- Logger.py
+++ Logger.py
@@ -2,7 +2,7 @@ import logging
 from datetime import datetime
 
 class Logger:
-    def log(self,file_name,request_str):
+    def log(file_name,request_str):
         logging.basicConfig(filename=file_name, level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
         current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         log_message = f"{current_time} - {request_str}"
